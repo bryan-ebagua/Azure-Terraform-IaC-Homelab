64de6225e078fee8423e5a036b186605714b04f0
Bryan Ebagua <bryanebagua@gmail.com>
2026-07-02 23:26:59 -0400

    Implemented network security group

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -22,5 +22,29 @@ resource "azurerm_subnet" "private" {
     address_prefixes = var.private_subnet_prefix
 }
   
+#Network Security Group
+resource "azurerm_network_security_group" "public" {
+    name = "public-nsg"
+    location = var.location
+    resource_group_name = var.resource_group_name
 
+    # Allow List
+    security_rule {
+        name                       = "Allow-HTTP-Inbound"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "Internet"
+        destination_address_prefix = "*"
+  }
+}
+
+# Assigns the Security Group to the subnet
+resource "azurerm_subnet_network_security_group_association" "public_assoc" {
+  subnet_id                 = azurerm_subnet.public.id
+  network_security_group_id = azurerm_network_security_group.public.id
+}
   
